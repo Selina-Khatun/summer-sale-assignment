ee0b77030aaf0c964651616020defb51b6594087
Md Tasdid Nayem <selinakhatun982520@gmail.com>
2023-08-20 22:44:50 +0600

    added discount calculation

---
+++ js/button.js
@@ -0,0 +1,24 @@
+function applyDiscount() {
+    const totalPrice =document.getElementById("total-amount").textContent;
+    // const totalPrice = document.getElementById("total-price");
+    // const total=totalPrice.innerText;
+    console.log(totalPrice);
+    // const couponCodeInput = document.getElementById("coupon-code");
+    // const applyButton = document.getElementById("apply-button");
+    // couponCodeInput.addEventListener('input', function () {
+    //     if (couponCodeInput.value === 'SELL200') {
+    //         applyButton.disabled = totalPrice < 200;
+    //     } else {
+    //         applyButton.disabled = true;
+    //     }
+    // });
+}
+applyDiscount()
+// console.log("selina")
+
+   // const totalAmount = document.getElementById("total-amount");
+    // const h4 = document.createElement('h4');
+    // h4.innerText = totalPrice;
+    // totalAmount.appendChild(h4);
+    // console.log(h4)
+    // return totalPrice;
--- js/index.js
+++ js/index.js
@@ -1,16 +1,46 @@
-let total=0;
+let totalAmount = 0;
+// let discountedTotal=0;
 function handleClick(target) {
     const selectedItemContainer = document.getElementById("selected-item");
     const itemName = target.childNodes[3].childNodes[3].innerText;
-    // console.log(target.childNodes[3].childNodes[3].innerText)
- 
     const li = document.createElement("li");
     li.innerText = itemName;
     selectedItemContainer.appendChild(li);
-    // console.log( target.childNodes[3].childNodes[5].innerText.split(" ")[0]);
-const price =target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+    const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
 
-total=parseFloat(total)+parseFloat(price);
+    totalAmount = parseFloat(totalAmount) + parseFloat(price);
 
-document.getElementById("total-price").innerText=total;
-}
+    //  document.getElementById("total-price").innerText = total;
+
+    const totalPrice = document.getElementById("total-price").innerText = totalAmount;
+    // console.log(totalPrice);
+    const couponCodeInput = document.getElementById("coupon-code");
+
+    // console.log(couponCodeInput);
+    const applyButton = document.getElementById("apply-button");
+    // console.log(applyButton);
+    couponCodeInput.addEventListener('input', function () {
+        if (couponCodeInput.value === 'SELL200') {
+            applyButton.disabled = totalPrice < 200;
+        } else {
+            applyButton.disabled = true;
+        }
+    });
+    applyButton.addEventListener('click', function () {
+        if (!applyButton.disabled) {
+            const discount = 0.20;
+            let discountedTotal = 0;
+            discountedTotal = totalPrice * discount;
+             document.getElementById("Discounted").innerText = discountedTotal;
+           const total= totalPrice-(totalPrice * discount);
+        //    return total;
+        console.log(total);
+
+        }
+    });
+};
+
+
+
+
+

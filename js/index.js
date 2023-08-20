2bab3cfc06f8aa17805ce2a3bc2e68ff81d96b31
Md Tasdid Nayem <selinakhatun982520@gmail.com>
2023-08-20 18:49:58 +0600

    total price calculation done

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
+let total=0;
 function handleClick(target) {
     const selectedItemContainer = document.getElementById("selected-item");
     const itemName = target.childNodes[3].childNodes[3].innerText;
     // console.log(target.childNodes[3].childNodes[3].innerText)
-
+ 
     const li = document.createElement("li");
     li.innerText = itemName;
     selectedItemContainer.appendChild(li);
-    // console.log(itemName)
+    // console.log( target.childNodes[3].childNodes[5].innerText.split(" ")[0]);
+const price =target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+
+total=parseFloat(total)+parseFloat(price);
+
+document.getElementById("total-price").innerText=total;
 }
